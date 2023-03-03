a0b3faf56934cb7642404e0666ad08a2c88b63a0
Tinsay <111936718+tinsayg@users.noreply.github.com>
2023-03-02 22:08:06 -0600

    Minor Changes

--- Lizard.cs
+++ Lizard.cs
@@ -1,8 +1,9 @@
-﻿//Written by
-//     Mikiyas Yoseph
-//     Niftalem Kassa
-//     Tinsay Gebremariam
-//on 3/2/23
+﻿/*Written by
+     Mikiyas Yoseph
+     Niftalem Kassa
+     Tinsay Gebremariam
+Last Mofified on 3/2/23*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
